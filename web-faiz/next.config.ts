f4649626840db95639574221470e7aa3eb5a4df4
Azenw12 <151103987+Azenw12@users.noreply.github.com>
2026-06-10 08:25:01 +0700

    fix css broken

--- web-faiz/next.config.ts
+++ web-faiz/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/portofolio-muhammadfaizfathurrahman',
+  basePath: '/portofolio-muhammadfaizfathurrahman', // <-- Ini yang bikin CSS lu balik
   images: {
     unoptimized: true,
   },
