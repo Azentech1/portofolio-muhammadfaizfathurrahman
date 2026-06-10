064b108027e17bcc49b79e8f5dde92f2d99f2820
Azenw12 <151103987+Azenw12@users.noreply.github.com>
2026-06-10 08:42:40 +0700

    tambahin nojekyll via actions

--- web-faiz/next.config.ts
+++ web-faiz/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portofolio-muhammadfaizfathurrahman',
-  assetPrefix: '/portofolio-muhammadfaizfathurrahman', // <-- Tambahin ini biar Tailwind lu kepanggil
   images: {
     unoptimized: true,
   },
